43a818bcc969fdaf96454d3d89db47e3b0a68775
aaronc <aaronclevette@gmail.com>
2023-10-06 22:24:15 -0500

    fixes

---
+++ assets/tiles/darkarea.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="darkarea" tilewidth="896" tileheight="448" tilecount="1" columns="1">
+ <image source="darkarea.png" width="896" height="448"/>
+</tileset>
