22c2adf5c37862b677e30580a406ecf16783274d
aaronc <aaronclevette@gmail.com>
2023-10-06 21:47:25 -0500

    fixes

---
+++ assets/tiles/screen-border-full.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="screen-border-full" tilewidth="137" tileheight="80" tilecount="10" columns="5">
+ <image source="screen-border-full.png" width="685" height="160"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="3" duration="500"/>
+   <frame tileid="4" duration="500"/>
+   <frame tileid="5" duration="500"/>
+   <frame tileid="6" duration="500"/>
+   <frame tileid="7" duration="500"/>
+   <frame tileid="8" duration="500"/>
+   <frame tileid="9" duration="500"/>
+  </animation>
+ </tile>
+</tileset>
