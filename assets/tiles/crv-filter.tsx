4938ebfec00c67ea16b8c1d0e7bd7337cd764ac9
aaronc <aaronclevette@gmail.com>
2023-10-06 21:35:00 -0500

    fixes

---
+++ assets/tiles/crv-filter.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="crv-filter" tilewidth="75" tileheight="45" tilecount="2" columns="2">
+ <grid orientation="orthogonal" width="68" height="43"/>
+ <image source="crv-filter.png" width="150" height="45"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="1" duration="500"/>
+  </animation>
+ </tile>
+</tileset>
--- assets/tiles/screen-border.tsx
+++ assets/tiles/screen-border.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="screen-border" tilewidth="130" tileheight="78" tilecount="1" columns="1" objectalignment="top">
- <image source="screen-border.png" width="130" height="78"/>
+<tileset version="1.8" tiledversion="1.8.6" name="screen-border" tilewidth="137" tileheight="81" tilecount="1" columns="1" objectalignment="top">
+ <grid orientation="orthogonal" width="130" height="78"/>
+ <image source="screen-border.png" width="137" height="81"/>
 </tileset>
