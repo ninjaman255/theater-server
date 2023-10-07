de82691e8302038b72f611114162e0d27bb1744f
aaronc <aaronclevette@gmail.com>
2023-10-06 21:10:37 -0500

    added movie backdrop

---
+++ assets/tiles/screen-border.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="screen-border" tilewidth="130" tileheight="78" tilecount="1" columns="1" objectalignment="top">
+ <image source="screen-border.png" width="130" height="78"/>
+</tileset>
